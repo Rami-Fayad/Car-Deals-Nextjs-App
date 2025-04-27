bf332950063f374f861671b52a6da636604a7656
Rami-Fayad <52130327@students.liu.edu.lb>
2025-04-27 15:41:01 +0300

    Car Catalogue Starter

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
     
       <main className="overflow-hidden">
         <Hero />
+        <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+        </div>
       </main>
       
    
